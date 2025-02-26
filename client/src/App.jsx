4ef741706fca7b5e4103781ff138134d65ea0a44
dhruti bhimani <bhimanidhruti83@gmail.com>
2025-02-26 17:02:53 +0530

    Created main landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignInPage from "./pages/SignInPage";
 import HomePage from "./pages/HomePage"; // Create this for the main app/dashboard
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignInPage />} />
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/signin" element={<SignInPage />} />
         <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
---
+++ client/src/components/button/CustomButton.jsx
@@ -0,0 +1,23 @@
+import React, { Children } from "react";
+import { Button } from "@mui/material";
+import "tailwindcss";
+import colors from "../../theme/colors";
+
+const CustomButton = ({
+    type = "button",
+    children,
+    sx = {},
+    onClick
+}) => {
+    return (
+        <Button type={type} onClick={onClick} variant="contained" sx={{
+            backgroundColor: colors.primary,
+            width: "150px",
+            fontFamily: "'Poppins', sans-serif",
+            ...sx,
+        }}>{children}</Button>
+
+    );
+}
+
+export default CustomButton;
